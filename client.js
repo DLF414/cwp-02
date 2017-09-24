b07985ea2b4a794b2bbe96f314ee3e8850757deb
DLF414 <Dmitry220198@gmail.com>
2017-09-24 23:04:24 +0300

    qa read + randomize

--- client.js
+++ client.js
@@ -1,14 +1,25 @@
 // client.js
 const clientReqest = "QA";
-const serverAccept = "ACK"
-const serverDecline = "DEC"
+const serverAccept = "ACK";
+const serverDecline = "DEC";
+const fs = require('fs');
 const net = require('net');
+const qaFile = "./qa.json";
 const port = 8124;
 
 const client = new net.Socket();
 
 client.setEncoding('utf8');
-
+fs.readFile(qaFile, function (err, data) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        questions = JSON.parse(data);
+        randomize();
+        console.log(questions);
+    }
+});
 client.connect(port, function() {
     console.log('Connected');
     client.write(clientReqest);
@@ -31,3 +42,13 @@ client.on('data', function(data) {
 client.on('close', function() {
     console.log('Connection closed');
 });
+function randomize() {
+    let counter = questions.length;
+    while (counter > 0) {
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+        let buf = questions[counter];
+        questions[counter] = questions[index];
+        questions[index] = buf;
+    }
+}
